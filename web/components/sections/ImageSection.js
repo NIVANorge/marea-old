a2be351968a4c0431a5074a65798f476e4c5f8a5
Jemmima Knight <jemmimaknight@gmail.com>
2023-02-03 19:00:58 +0100

    Remove image section title box

--- web/components/sections/ImageSection.js
+++ web/components/sections/ImageSection.js
@@ -24,12 +24,10 @@ function ImageSection(props) {
         />
         <figcaption>
           <div className={styles.caption}>
-            <div className={styles.captionBox}>
-              <div className={styles.label}>{label}</div>
-              <h2 className={styles.title}>{heading}</h2>
-              {text && <SimpleBlockContent blocks={text} />}
-              {cta && cta.route && <Cta {...cta} />}
-            </div>
+            <div className={styles.label}>{label}</div>
+            <h2 className={styles.title}>{heading}</h2>
+            {text && <SimpleBlockContent blocks={text} />}
+            {cta && cta.route && <Cta {...cta} />}
           </div>
         </figcaption>
       </figure>
