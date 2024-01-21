8e70d32fd9304ee7fd107fbbc9c2419effc2537a
Jemmima Knight <jemmimaknight@gmail.com>
2024-01-21 22:18:41 +0100

    Remove deprecated css nesting

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -2,11 +2,6 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     cssnano: true,
-    'postcss-preset-env': {
-      stage: 0,
-      features: {
-        'nesting-rules': true,
-      },
-    },
+    'postcss-preset-env': {},
   },
 }
